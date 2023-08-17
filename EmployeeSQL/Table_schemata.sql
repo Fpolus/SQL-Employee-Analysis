d49148a21ecf7f80f0b5718136f6670d992b53f8
Frank <frank.polus20@gmail.com>
2023-08-16 21:51:54 -0700

    Turn In

--- EmployeeSQL/Table_schemata.sql
+++ EmployeeSQL/Table_schemata.sql
@@ -1,11 +1,11 @@
 CREATE TABLE employee (
-	emp_no INT NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
     PRIMARY KEY (emp_no)
 );
 
@@ -14,30 +14,30 @@ CREATE TABLE salaries (
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employee (emp_no)
 );
-	
+
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(100) NOT NULL,
     PRIMARY KEY (dept_no)
 );
-	
+
 CREATE TABLE dept_managers (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
     emp_no INT NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     FOREIGN KEY (emp_no) REFERENCES employee (emp_no)
 );
-	
+
 CREATE TABLE department_employees (
     emp_no INT NOT NULL,
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
     PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employee(emp_no)
 );
 
 CREATE TABLE titles (
-    title_id VARCHAR NOT NULL,
-    title VARCHAR NOT NULL,
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     PRIMARY KEY (title_id)
 );
